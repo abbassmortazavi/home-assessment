635d5d5a33975264d957dde36752ae43c4e8499a
abbass <abbassmortazavi42@gmail.com>
2024-11-04 13:40:31 +0330

    fix:add api platform

--- src/Entity/Company.php
+++ src/Entity/Company.php
@@ -2,12 +2,27 @@
 
 namespace App\Entity;
 
+use ApiPlatform\Metadata\ApiResource;
+use ApiPlatform\Metadata\Get;
+use ApiPlatform\Metadata\GetCollection;
+use ApiPlatform\Metadata\Post;
 use App\Repository\CompanyRepository;
 use Doctrine\Common\Collections\ArrayCollection;
 use Doctrine\Common\Collections\Collection;
 use Doctrine\ORM\Mapping as ORM;
+
 use Symfony\Component\Validator\Constraints as Assert;
 #[ORM\Entity(repositoryClass: CompanyRepository::class)]
+#[ApiResource(
+    operations: [
+        new GetCollection(),
+        new Get(),
+        new Post(
+            security: "is_granted('ROLE_SUPER_ADMIN')",
+            securityMessage: "Only SUPER ADMIN can create companies."
+        ),
+    ]
+)]
 class Company
 {
     #[ORM\Id]
